b9431cff65d62f8f095c8a15b3cedcbce6a2688b
Teddy <basseyteddy084@gmail.com>
2026-09-07 02:01:16 +0100

    Seed NGN currency on deploy

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "from pheral.models import Currency; Currency.objects.get_or_create(code='NGN', defaults={'name':'Nigerian Naira','symbol':'₦','is_active':True,'decimal_places':2})"
